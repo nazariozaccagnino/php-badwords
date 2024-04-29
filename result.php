dda2665e3d96293036d962529646b0406b91d80c
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-04-29 16:45:21 +0200

    added the <p> length printing

--- result.php
+++ result.php
@@ -2,10 +2,11 @@
 //var_dump($_POST);
 $textarea = $_POST['textarea'];
 $badword = $_POST['badword'];
-$modifiedtext = str_replace("$badword","***", "$textarea");
+$modifiedtext = str_replace("$badword", "***", "$textarea");
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,6 +15,7 @@ $modifiedtext = str_replace("$badword","***", "$textarea");
     <link rel="stylesheet" href="./css/styles.css">
     <title>Result</title>
 </head>
+
 <body>
     <div class="container">
         <div>
@@ -21,13 +23,21 @@ $modifiedtext = str_replace("$badword","***", "$textarea");
             <p>
                 <?php echo $textarea ?>
             </p>
+            <div>
+            <h5 class="text-danger">La lunghezza totale del paragrafo è <?php echo strlen($textarea)?></h5> 
+
+            </div>
         </div>
         <div>
             <h3>Paragrafo censurato</h3>
             <p>
                 <?php echo $modifiedtext ?>
             </p>
+            <div>
+               <h5 class="text-danger">La lunghezza totale del paragrafo è <?php echo strlen($modifiedtext)?></h5> 
+            </div>
         </div>
     </div>
 </body>
+
 </html>
